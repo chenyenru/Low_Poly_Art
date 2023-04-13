d425a522ff504cad8b30c39a910517defc33a000
ChenYenRu <rebecca946487@gmail.com>
2023-04-13 08:29:05 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import os
+import numpy as np
+import pygame
+import pygame.gfxdraw
+import matplotlib.pyplot as plt
+from scipy.spatial import Delaunay
+from scipy.ndimage import gaussian_filter
+from collections import defaultdict
+
